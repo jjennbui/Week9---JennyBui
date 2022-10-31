adac76e3dd46d1ba5577cb951d0cbb46ed149e49
jjennbui <113149333+jjennbui@users.noreply.github.com>
2022-10-31 10:25:24 -0700

    Add files via upload

---
+++ week9/For-Loop/for-in-loop.js
@@ -0,0 +1,15 @@
+var computer = {brand: "microsoft", laptop: " surface laptop 4", screen: "13.5\""};
+var sentence = "I have the: ";
+
+for(var item in computer){
+    if(item == "brand") {
+    sentence += computer.brand;
+} else if (item=="laptop"){
+    sentence+= computer.laptop + ".";
+    break;
+}
+if(item == "brand" || item == "laptop"){
+    sentence =+ "";
+}
+}
+console.log(sentence);
---
+++ week9/Switch/switch.js
@@ -0,0 +1,14 @@
+var car = 0;
+var text = "";
+
+switch(car) {
+    case 0:
+        text = "off";
+        break;
+        case 1:
+            test = "on";
+            break;
+        default:
+            text = "no value found";    
+}
+ console.log("The car is " + text);
